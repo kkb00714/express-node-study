b60bee767cbc135035b052f13a70c41b2e8a1d44
kkb <0furiousblade0@gmail.com>
2024-03-08 16:01:09 +0900

    [#09] Error Handling

--- rest-api-practice/src/index.js
+++ rest-api-practice/src/index.js
@@ -62,6 +62,15 @@ app.get("/", (req, res) => {
     res.send("Node.js 화이팅");
 });
 
+app.use((err, req, res, next) => {
+    console.log(err);
+
+    res
+    .status(err.status || 500)
+    .json({ message :  err.message || "서버에서 에러가 발생했습니다."});
+
+});
+
 app.listen(8000, (req, res) => {
     console.log("서버가 실행됩니다.");
 });
