dd02a5d6d59d3d31e1d081285e68b7e796511f04
kkb <0furiousblade0@gmail.com>
2024-03-25 14:13:21 +0900

    [#36] express-react 연동_4 (DB x) - 유저 회원가입, 메인페이지 분리

--- 07_express-board/my-app/src/App.js
+++ 07_express-board/my-app/src/App.js
@@ -1,79 +1,27 @@
-// import Register from './Register';
-import { useEffect, useState } from 'react';
 
-function App() {
-  const [getUsers, setGetUsers] = useState(null); 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Register from './Register';
 
-  const fetchData = () => {
-    // 등록 후에 사용자 목록 다시 가져오기
-    fetch('http://localhost:8000/users/list')
-      .then((response) => response.json())
-      .then((data) => setGetUsers(data));
-  };
-
-  useEffect(() => {fetchData()}, []);
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    const id = e.target.id.value;
-    const password = e.target.password.value;
-    const name = e.target.name.value;
-    const age = e.target.age.value;
-    const email = e.target.email.value;
-    const phoneNumber = e.target.phoneNumber.value;
-
-    fetch('http://localhost:8000/users/register', {
-      method: 'POST',
-      body: JSON.stringify({
-        id,
-        password,
-        name,
-        age,
-        email,
-        phoneNumber,
-      }),
-
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(() => fetchData());
-  };
-
+function Main() {
   return (
     <div className="App">
-
-      <h2>리액트 회원가입 화면</h2>
-      
-      <form onSubmit={onSubmitHandler}>
-        <input name="id"></input>
-        <br/>
-        <input name="password"></input>
-        <br/>
-        <input name="name"></input>
-        <br/>
-        <input name="age"></input>
-        <br/>
-        <input name="email"></input>
-        <br/>
-        <input name="phoneNumber"></input>
-        <br/>
-        <input type="submit" value="유저 등록"></input>
-      </form>
-
-      {/* {getUsers?.map((user) => (
-        <div key={user.id}>
-          <div>{user.id}</div>
-          <div>{user.password}</div>
-          <div>{user.name}</div>
-          <div>{user.age}</div>
-          <div>{user.email}</div>
-          <div>{user.phoneNumber}</div>
-        </div>
-  
-      ))} */}
-
+      <h2>메인 페이지</h2>
+      <Link to="/register">회원가입 페이지로 이동</Link>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- 07_express-board/my-app/src/Register.js
+++ 07_express-board/my-app/src/Register.js
@@ -1,86 +1,68 @@
-// import React, { Component } from 'react';
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-// class Register extends Component {
-//   state = {
-//     username: '',
-//     password: '',
-//     name: '',
-//     age: '',
-//     email: '',
-//     phoneNumber: '',
-//     loading: false,
-//     error: ''
-//   };
+function Register() {
+    const [getUsers, setGetUsers] = useState(null); 
+    const navigate = useNavigate();
 
-//   handleChange = (e) => {
-//     this.setState({
-//       [e.target.name]: e.target.value
-//     });
-//   };
+    const fetchData = () => {
+      // 등록 후에 사용자 목록 다시 가져오기
+        fetch('http://localhost:8000/users/list')
+            .then((response) => response.json())
+            .then((data) => setGetUsers(data));
+    };
 
-//   handleSubmit = async (e) => {
-//     e.preventDefault();
-//     this.setState({ loading: true, error: '' });
+    useEffect(() => {fetchData()}, []);
 
-//     const { username, password, name, age, email, phoneNumber } = this.state;
-//     const formData = { username, password, name, age, email, phoneNumber };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        const id = e.target.id.value;
+        const password = e.target.password.value;
+        const name = e.target.name.value;
+        const age = e.target.age.value;
+        const email = e.target.email.value;
+        const phoneNumber = e.target.phoneNumber.value;
+    
+        fetch('http://localhost:8000/users/register', {
+            method: 'POST',
+            body: JSON.stringify({
+            id,
+            password,
+            name,
+            age,
+            email,
+            phoneNumber,
+        }),
 
-//     try {
-//       const response = await axios.post('/users/register', formData);
-//       console.log(response.data);
-//       window.location = '/users/list';
-//     } catch (error) {
-//       console.error('회원가입에 실패했습니다.', error);
-//       this.setState({ error: '회원가입에 실패했습니다. 다시 시도해주세요.' });
-//     } finally {
-//       this.setState({ loading: false });
-//     }
-//   };
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(() => fetchData());
+    navigate('/') // 회원가입 후 메인페이지로 이동
+};
 
-//   render() {
-//     const { username, password, name, age, email, phoneNumber, loading, error } = this.state;
+    return (
+        <div className="App">
+            <h2>리액트 회원가입 화면</h2>
+        
+            <form onSubmit={onSubmitHandler}>
+                <input name="id"></input>
+                <br/>
+                <input name="password"></input>
+                <br/>
+                <input name="name"></input>
+                <br/>
+                <input name="age"></input>
+                <br/>
+                <input name="email"></input>
+                <br/>
+                <input name="phoneNumber"></input>
+                <br/>
+                <input type="submit" value="유저 등록"></input>
+            </form>
+        </div>
+    );
+}
 
-//     return (
-//       <div>
-//         <h2>회원가입 페이지</h2>
-//         <form onSubmit={this.handleSubmit}>
-//           <label>
-//             Username:
-//             <input type="text" name="username" value={username} onChange={this.handleChange} />
-//           </label>
-//           <br />
-//           <label>
-//             Password:
-//             <input type="password" name="password" value={password} onChange={this.handleChange} />
-//           </label>
-//           <br />
-//           <label>
-//             Name:
-//             <input type="text" name="name" value={name} onChange={this.handleChange} />
-//           </label>
-//           <br />
-//           <label>
-//             Age:
-//             <input type="number" name="age" value={age} onChange={this.handleChange} />
-//           </label>
-//           <br />
-//           <label>
-//             Email:
-//             <input type="email" name="email" value={email} onChange={this.handleChange} />
-//           </label>
-//           <br />
-//           <label>
-//             Phone Number:
-//             <input type="text" name="phoneNumber" value={phoneNumber} onChange={this.handleChange} />
-//           </label>
-//           <br />
-//           {error && <div style={{ color: 'red' }}>{error}</div>}
-//           <button type="submit" disabled={loading}>회원가입</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Register;
+export default Register;
